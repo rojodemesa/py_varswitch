c159c978ca67b0885fa6fcacdacaa6628cf28020
rojodemesa <98437854+rojodemesa@users.noreply.github.com>
2022-06-06 09:19:59 +0800

    Add files via upload

---
+++ varswitch.py
@@ -0,0 +1,17 @@
+# Dont change the code below
+a = input("a:")
+b = input("b:")
+# Dont change
+
+# write code below here ##
+tempo = a
+a = b
+b = tempo
+
+
+#  Dont change the code below
+print("a = " + a)
+print("b = " + b)
+
+
+
